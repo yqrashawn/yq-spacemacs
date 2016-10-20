fcfdf18a0c56c1321c469e5c6f79ab8af2ac0147
Aron Griffis <aron@scampersand.com>
2016-10-19 20:29:06 -0400

    Add ripgrep support
    * Add rg to helm completion layer
    * Add rg to ivy layer
    * Prepend rg to dotspacemacs-search-tools
    * Add rg to documentation

--- layers/+completion/ivy/funcs.el
+++ layers/+completion/ivy/funcs.el
@@ -172,6 +172,7 @@ that directory."
 ;; Define search functions for each tool
 (cl-loop
    for (tools tool-name) in '((dotspacemacs-search-tools "auto")
+                              ((list "rg") "rg")
                               ((list "ag") "ag")
                               ((list "pt") "pt")
                               ((list "ack") "ack")
